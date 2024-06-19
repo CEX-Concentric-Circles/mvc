437e7d04622e816e4ea6d12c89eeadf0eaa888b5
tmagsi <tariq.magsi@evolute.app>
2024-06-19 09:28:08 +0200

    project setup

---
+++ WarehouseManagementMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WarehouseManagementMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
